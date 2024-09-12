ca01115792a5e131bfe0bbe6b7e6c0be47f1e812
Alex Fernandes <alexfernands@outlook.com>
2024-09-12 01:51:47 -0400

    style and animation to click to buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./Button.css";
 
 export default function Button(props) {
-  return <button className="button">{props.label}</button>;
+  // Build the class names based on the props
+  const classes = [
+    'button',
+    props.operation ? 'operation' : '',
+    props.double ? 'double' : '',
+    props.triple ? 'triple' : ''
+  ].join(' ').trim();
+
+  return (
+    <button className={classes} onClick={props.onClick}>
+      {props.label}
+    </button>
+  );
 }
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,29 +4,47 @@ import Button from "../components/Button";
 import Display from "../components/Display";
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+
+  clearMemory() {
+    console.log("clean");
+  }
+
+  setOperation() {
+    console.log("operation");
+  }
+
+  addDigit(n) {
+    console.log(n);
+  }
+
   render() {
     return (
       <div className="calculator">
-        <Display value={100}/>
-        <Button label="AC"/>
-        <Button label="+/-"/>
-        <Button label="%"/>
-        <Button label="/"/>
-        <Button label="7"/>
-        <Button label="8"/>
-        <Button label="9"/>
-        <Button label="*"/>
-        <Button label="4"/>
-        <Button label="5"/>
-        <Button label="6"/>
-        <Button label="-"/>
-        <Button label="1"/>
-        <Button label="2"/>
-        <Button label="3"/>
-        <Button label="+"/>
-        <Button label="0"/>
-        <Button label="."/>
-        <Button label="="/>
+        <Display value={100} />
+        <Button label="AC" onClick={this.clearMemory} double />
+        <Button label="%" />
+        <Button label="/" onClick={() => this.setOperation('/')} operation />
+        <Button label="7" onClick={() => this.addDigit('7')} />
+        <Button label="8" onClick={() => this.addDigit('8')} />
+        <Button label="9" onClick={() => this.addDigit('9')} />
+        <Button label="*" onClick={() => this.setOperation('*')} operation />
+        <Button label="4" onClick={() => this.addDigit('4')} />
+        <Button label="5" onClick={() => this.addDigit('5')} />
+        <Button label="6" onClick={() => this.addDigit('6')} />
+        <Button label="-" onClick={() => this.setOperation('-')} operation />
+        <Button label="1" onClick={() => this.addDigit('1')} />
+        <Button label="2" onClick={() => this.addDigit('2')} />
+        <Button label="3" onClick={() => this.addDigit('3')} />
+        <Button label="+" onClick={() => this.setOperation('+')} operation />
+        <Button label="0" onClick={() => this.addDigit('0')} double/>
+        <Button label="." onClick={() => this.addDigit('.')} />
+        <Button label="=" onClick={() => this.setOperation} operation/>
       </div>
     );
   }
