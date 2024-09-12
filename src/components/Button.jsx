6b8a3250b740ae7e7111871a0b48e9ecec9876d2
Alex Fernandes <alexfernands@outlook.com>
2024-09-11 23:58:11 -0400

    calc buttons created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./Button.css";
 
 export default function Button(props) {
-  return <button className="button">0</button>;
+  return <button className="button">{props.label}</button>;
 }
