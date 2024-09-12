da992d6d3d43b37f68f4c7fd5ba250b01d272f85
Alex Fernandes <alexfernands@outlook.com>
2024-09-11 23:43:48 -0400

    calc buttons created

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./Calculator.css";
+import Button from "../components/Button";
 
 export default class Calculator extends Component {
   render() {
     return (
       <div className="calculator">
         <h1>Hello World!</h1>
+        <Button />
       </div>
     );
   }
