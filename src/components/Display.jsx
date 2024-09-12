bf2ca1b6b4435e79f0d55fb8a73a6bc25c1bc874
Alex Fernandes <alexfernands@outlook.com>
2024-09-12 00:55:35 -0400

    created calculator display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Display.css";
 
 export default function Display(props) {
-  return;
-  <div className="display">{props.value}</div>;
+  return (
+    <div className="display">{props.value}</div>
+  );
 }
+
